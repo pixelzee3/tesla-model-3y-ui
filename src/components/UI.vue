b72a6ce6954789a90ea985f094060e86bf210b01
pixelzee3 <bpixelzee@gmail.com>
2022-04-25 21:50:52 +0700

    Change flex basis for UI grid layout.

--- src/components/UI.vue
+++ src/components/UI.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="min-w-[48rem] h-screen overflow-auto flex flex-col flex-nowrap">
-    <div class="flex basis-11/12">
+    <div class="flex basis-5/6">
       <div class="w-full h-full bg-blue-200 basis-2/5"></div>
       <div class="w-full h-full bg-red-200 basis-3/5"></div>
     </div>
-    <div class="w-full h-full bg-green-200 basis-1/12"></div>
+    <div class="w-full h-full bg-green-200 basis-1/6"></div>
   </div>
 </template>
