e90f7980e3020c632518cf9b4e197bd8a9bc05d8
pixelzee3 <bpixelzee@gmail.com>
2022-04-25 20:56:40 +0700

    Reset Grid.

--- src/components/MainGrid.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="w-full h-full grid grid-cols-4">
-    <slot />
-  </div>
-</template>
--- src/components/UI.vue
+++ src/components/UI.vue
@@ -1,13 +1,11 @@
 <script setup>
-  import MainGrid from './MainGrid.vue'
+import MainGrid from './MainGrid.vue';
 </script>
 
 <template>
   <div class="min-w-[48rem] h-screen overflow-auto">
-    <MainGrid>
-      <div class="w-full h-full bg-blue-200"></div>
-      <div class="w-full h-full bg-red-200"></div>
-      <div class="w-full h-full bg-green-200"></div>
-    </MainGrid>
+    <div class="w-full h-full bg-blue-200"></div>
+    <div class="w-full h-full bg-red-200"></div>
+    <div class="w-full h-full bg-green-200"></div>
   </div>
 </template>
