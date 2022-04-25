2ae6949224dd39db252f6ab3a601183c50d7af2d
pixelzee3 <bpixelzee@gmail.com>
2022-04-25 20:15:42 +0700

    Define grid property for UI layout.

--- src/components/UI.vue
+++ src/components/UI.vue
@@ -1,3 +1,13 @@
+<script setup>
+  import MainGrid from './MainGrid.vue'
+</script>
+
 <template>
-  <div class="min-w-[48rem] overflow-auto"></div>
+  <div class="min-w-[48rem] h-screen overflow-auto">
+    <MainGrid>
+      <div class="w-full h-full bg-blue-200"></div>
+      <div class="w-full h-full bg-red-200"></div>
+      <div class="w-full h-full bg-green-200"></div>
+    </MainGrid>
+  </div>
 </template>
