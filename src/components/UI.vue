d01906409a25664c2602d72f150d83607b899c3a
pixelzee3 <bpixelzee@gmail.com>
2022-04-25 21:50:12 +0700

    Define basic grid layout.

--- src/components/UI.vue
+++ src/components/UI.vue
@@ -1,11 +1,9 @@
-<script setup>
-import MainGrid from './MainGrid.vue';
-</script>
-
 <template>
-  <div class="min-w-[48rem] h-screen overflow-auto">
-    <div class="w-full h-full bg-blue-200"></div>
-    <div class="w-full h-full bg-red-200"></div>
-    <div class="w-full h-full bg-green-200"></div>
+  <div class="min-w-[48rem] h-screen overflow-auto flex flex-col flex-nowrap">
+    <div class="flex basis-11/12">
+      <div class="w-full h-full bg-blue-200 basis-2/5"></div>
+      <div class="w-full h-full bg-red-200 basis-3/5"></div>
+    </div>
+    <div class="w-full h-full bg-green-200 basis-1/12"></div>
   </div>
 </template>
