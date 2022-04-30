0fca9493e79820e205a96419aeb9adb631f8b15e
pixelzee3 <bpixelzee@gmail.com>
2022-04-30 22:33:41 +0700

    Add Head data and favicon.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,16 @@ import { defineNuxtConfig } from 'nuxt';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'Tesla Model 3/Y UI Simulator',
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: 'public/favicon.svg',
+        },
+      ],
+    },
+  },
 });
